7cada6ae28fb11f7f0cb885e5dd7fc6b87352a5a
cycatzsri <109794249+cycatzsri@users.noreply.github.com>
2022-10-01 18:38:40 +0530

    Update banners.go

--- v2/pkg/runner/banners.go
+++ v2/pkg/runner/banners.go
@@ -9,25 +9,19 @@ import (
 	"github.com/projectdiscovery/subfinder/v2/pkg/resolve"
 )
 
-const banner = `
-               __    _____           __         
-   _______  __/ /_  / __(_)___  ____/ /__  _____
-  / ___/ / / / __ \/ /_/ / __ \/ __  / _ \/ ___/
- (__  ) /_/ / /_/ / __/ / / / / /_/ /  __/ /    
-/____/\__,_/_.___/_/ /_/_/ /_/\__,_/\___/_/ v2.5.3
-`
+const banner = ``
 
 // Version is the current version of subfinder
 const Version = `v2.5.3`
 
 // showBanner is used to show the banner to the user
 func showBanner() {
-	gologger.Print().Msgf("%s\n", banner)
-	gologger.Print().Msgf("\t\tprojectdiscovery.io\n\n")
+	gologger.Print().Msgf("", banner)
+	gologger.Print().Msgf("\t\t")
 
-	gologger.Print().Msgf("Use with caution. You are responsible for your actions\n")
-	gologger.Print().Msgf("Developers assume no liability and are not responsible for any misuse or damage.\n")
-	gologger.Print().Msgf("By using subfinder, you also agree to the terms of the APIs used.\n\n")
+	gologger.Print().Msgf("")
+	gologger.Print().Msgf("")
+	gologger.Print().Msgf("")
 }
 
 // loadProvidersFrom runs the app with source config
